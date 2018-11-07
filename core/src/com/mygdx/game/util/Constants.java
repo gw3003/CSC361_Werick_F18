82ad60b09b6e55c8338324dd311fe65ce727408e
Gabe <gw3003@cs.ship.edu>
2018-11-07 14:09:48 -0500

    Progress on displaying room

--- core/src/com/mygdx/game/util/Constants.java
+++ core/src/com/mygdx/game/util/Constants.java
@@ -19,5 +19,5 @@ public class Constants
 	public static final String LEVEL = "levels/level.dng";
 	
 	//Current level file version capable of being read
-	public static final float DNG_VERSION = 0.2f;
+	public static final String DNG_VERSION = "0.1";
 }
--- core/src/com/mygdx/game/world/Level.java
+++ core/src/com/mygdx/game/world/Level.java
@@ -1,6 +1,44 @@
 package com.mygdx.game.world;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.objects.DungeonBackground;
+import com.mygdx.game.util.Constants;
+
 public class Level
 {
-
+	public Array<DungeonBackground> rooms;
+	
+	public Level(String filename)
+	{
+		init(filename);
+	}
+	
+	private void init(String filename)
+	{
+		//open dng file
+		try
+		{
+			FileReader fr = new FileReader(Constants.LEVEL);
+			Scanner dng = new Scanner(fr);
+		} catch (FileNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+		
+		
+		
+		
+		//read how many rooms there are
+	}
+	
+	public void render(SpriteBatch batch)
+	{
+		
+	}
 }
